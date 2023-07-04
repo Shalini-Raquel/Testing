5f74788dd5ab7bf7d90173db6e39200d777911b2
Shalini-Raquel <134043753+Shalini-Raquel@users.noreply.github.com>
2023-07-04 17:05:59 +0530

    JDBC Practice Exercises

---
+++ Phase-1 Practice Project: Assisted Practice/ConnectionStatementResultSet.java
@@ -0,0 +1,34 @@
+package Demo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ConnectionStatementResultSet {
+	
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		String dbUrl = "jdbc:mysql://localhost:3306/movies"; //connection string
+		String username="root";
+		String password = "Shalini59@12*c";
+		String query = "Select * from movies;";
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection(dbUrl, username, password); //connecting DB
+		Statement stmt = con.createStatement(); //Execute the query
+		ResultSet rs =  (stmt).executeQuery(query); //Save the result after executing the query
+		
+		while (rs.next())
+		{
+			System.out.print("Title: " + rs.getString("title") + "\t");
+			System.out.print("Genre: " + rs.getString("genre") + "\t");
+			System.out.print("Director: " + rs.getString("Director") + "\t");
+			System.out.println("Release Year: " + rs.getString("release_year"));
+		}
+		
+		
+	}
+
+}
+
